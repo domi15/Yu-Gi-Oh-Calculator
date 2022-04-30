0c332759d725349a7b444e362cb1e989f6c4fae9
domi15 <domi15.dt@gmail.com>
2022-04-30 14:51:28 +0200

    added coin flip

---
+++ coinflip.js
@@ -0,0 +1,15 @@
+document.getElementById('click').onclick = click;
+
+var heads = 0;
+var tails = 0;
+function click() {  
+    x = (Math.floor(Math.random() * 2) == 0);
+    if(x){
+    	flip("heads");
+    }else{
+        flip("tails");
+    }
+};
+function flip(coin) {
+    document.getElementById("result").innerHTML = coin;
+};
