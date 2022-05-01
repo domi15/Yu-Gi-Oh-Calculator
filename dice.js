c11cad5cef8a466e8fbd13867cf147792bc3160f
domi15 <domi15.dt@gmail.com>
2022-05-01 10:58:34 +0200

    dice change

--- dice.js
+++ dice.js
@@ -1,23 +1,4 @@
-var dice = {
-    sides: 6,
-    roll: function () {
-      var randomNumber = Math.floor(Math.random() * this.sides) + 1;
-      return randomNumber;
-    }
-  }
-  
-  
-  
-  //Prints dice roll to the page
-  
-  function printNumber(number) {
-    var placeholder = document.getElementById('placeholder');
-    placeholder.innerHTML = number;
-  }
-  
-  var button = document.getElementById('button');
-  
-  button.onclick = function() {
-    var result = dice.roll();
-    printNumber(result);
-  };  
+function rollTheDice() {
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  document.getElementById("dice").innerHTML = randomNumber1
+}
